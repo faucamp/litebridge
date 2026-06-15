130b83bd6b4dc4f71957df7266e617b5e97426e0
Francois Aucamp <francois.aucamp@gmail.com>
2026-06-15 14:35:01 +0200

    Package javadocs added

---
+++ litebridge-orm/src/main/java/org/litebridgedb/orm/function/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Select query expressions/functions.
+ * <p>
+ * The primary purpose of this package is to provide a set of interfaces and
+ * classes for constructing and manipulating select expressions in database
+ * queries. These expressions are used to specify what data should be retrieved
+ * from the database.
+ * <p>
+ * The main entry point is the {@link org.litebridgedb.orm.function.Functions} utility class.
+ *
+ * @see org.litebridgedb.orm.function.Functions
+ */
+package org.litebridgedb.orm.function;
