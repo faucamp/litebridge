13b67beef4d1ee34419c2b12d9ac6643208e1353
Francois Aucamp <francois.aucamp@gmail.com>
2026-08-10 10:00:03 +0200

    Add javadocs

--- litebridge-orm/src/main/java/org/litebridge/orm/nativesql/NativeSqlCache.java
+++ litebridge-orm/src/main/java/org/litebridge/orm/nativesql/NativeSqlCache.java
@@ -3,10 +3,21 @@ package org.litebridge.orm.nativesql;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Cache for parsed named parameter-based queries.
+ */
 public final class NativeSqlCache {
 
     private final Map<String, ParsedSql> cache = new ConcurrentHashMap<>();
 
+    /**
+     * Retrieves a cached parsed SQL statement for the given raw SQL string.
+     * <p>
+     * If the SQL statement is not found in the cache, it is parsed and added to the cache.
+     *
+     * @param rawSql The raw SQL string to retrieve or parse.
+     * @return The parsed SQL statement.
+     */
     public ParsedSql getCachedSql(final String rawSql) {
         return cache.computeIfAbsent(rawSql, SqlParser::parseSql);
     }
