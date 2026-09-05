0616ea51b87f1040142b718f17ba2f8738bfea73
Francois Aucamp <francois.aucamp@gmail.com>
2026-09-05 18:49:00 +0200

    Cleanup

--- litebridge-orm/src/main/java/org/litebridge/orm/api/merge/MergeOnStep.java
+++ litebridge-orm/src/main/java/org/litebridge/orm/api/merge/MergeOnStep.java
@@ -2,10 +2,9 @@ package org.litebridge.orm.api.merge;
 
 import org.jspecify.annotations.Nullable;
 import org.litebridge.db.spi.query.LogicOperator;
-import org.litebridge.orm.api.delete.DtoDeleteWhereConditionClause;
+import org.litebridge.orm.engine.LitebridgeContext;
 import org.litebridge.orm.engine.ast.MergeNode;
 import org.litebridge.orm.engine.ast.UsingNode;
-import org.litebridge.orm.engine.LitebridgeContext;
 import org.litebridge.orm.expression.ExpressionSpec;
 
 import java.util.Objects;
@@ -44,20 +43,5 @@ public sealed class MergeOnStep<DTO, MUS extends MergeUpdateStep<DTO>>
                     final UsingNode usingNode = new UsingNode(mergeNode, usingTable, usingDtoClass, conditionNode);
                     return new MergeOnConditionClauseTerminal<>(mergeNode, usingNode, litebridgeContext);
                 });
-    }
-
-    DtoDeleteWhereConditionClause<DTO> whereImpl(final LogicOperator logicOperator, final String field) {
-//        final Column column = deleteSpec.dtoTable().getColumnForFieldName(field).toColumn();
-//        return whereImpl(logicOperator, new SelectColumnSpec(column));
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
-    DtoDeleteWhereConditionClause<DTO> whereImpl(final LogicOperator logicOperator, final ExpressionSpec expression) {
-//        final Function<QueryNode, DtoDeleteWhereConditionClauseTerminal<DTO>> recreator = n -> {
-//            this.node = new WhereNode(this.node, n);
-//            return new DtoDeleteWhereConditionClauseTerminalImpl<>(this);
-//        };
-//        return new DtoDeleteWhereConditionClause<>(litebridgeContext, logicOperator, expression, recreator);
-        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
